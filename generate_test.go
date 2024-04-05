c084301267be07a16d525ffd9ddecaa47118e57f
Dyam Marcano <dyam.marcano@gmail.com>
2024-04-04 22:34:45 -0300

    change the way to create version file

--- generate_test.go
+++ generate_test.go
@@ -2,8 +2,6 @@ package version
 
 import (
 	"github.com/stretchr/testify/assert"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
@@ -12,16 +10,5 @@ func TestNewVersion(t *testing.T) {
 	assert.NoError(t, err)
 
 	err = v.Generate(v.ProjectPath)
-	assert.NoError(t, err)
-
-	destPath := filepath.Join(v.ProjectPath, "version", "version.go")
-
-	if _, err = os.Stat(destPath); os.IsNotExist(err) {
-		if err = os.MkdirAll(destPath, os.ModePerm); err != nil {
-			assert.Error(t, err)
-		}
-	}
-
-	err = os.RemoveAll(destPath)
 	assert.NoError(t, err)
 }
